2f38e270cdbee1bea9d8ea88395d64063ab7d21d
Alexander Lee Smith <63920477+lapsangsouchy@users.noreply.github.com>
2024-06-17 13:38:39 -0400

    Added mouseup and mousedown handler functions

--- home.js
+++ home.js
@@ -12,6 +12,25 @@ function isPlaying(audio) {
   return !audio.paused;
 }
 
+const mousedownHandler = (changingImage) => {
+  changingImage.src = './assets/pikachuJump2.png';
+
+  if (isPlaying(audio2)) {
+    audio2.pause();
+    audio2.currentTime = 0;
+  }
+  audio1.play();
+};
+
+const mouseupHandler = (changingImage) => {
+  changingImage.src = './assets/pikachuJump1.png';
+  if (isPlaying(audio1)) {
+    audio1.pause();
+    audio1.currentTime = 0;
+  }
+  audio2.play();
+};
+
 const changeImage = (e) => {
   e.preventDefault();
 
@@ -26,25 +45,41 @@ const changeImage = (e) => {
     //Add new text
     instruction.style.visibility = 'visible';
 
-    // Onmousedown
     changingImage.onmousedown = () => {
-      changingImage.src = './assets/pikachuJump2.png';
+      mousedownHandler(changingImage);
+    };
 
-      if (isPlaying(audio2)) {
-        audio2.pause();
-        audio2.currentTime = 0;
-      }
-      audio1.play();
-    };
-    // Onmouseup
     changingImage.onmouseup = () => {
-      changingImage.src = './assets/pikachuJump1.png';
-      if (isPlaying(audio1)) {
-        audio1.pause();
-        audio1.currentTime = 0;
-      }
-      audio2.play();
+      mouseupHandler(changingImage);
     };
+
+    instruction.onmousedown = () => {
+      mousedownHandler(changingImage);
+    };
+
+    instruction.onmouseup = () => {
+      mouseupHandler(changingImage);
+    };
+
+    // // Onmousedown
+    // changingImage.onmousedown = () => {
+    //   changingImage.src = './assets/pikachuJump2.png';
+
+    //   if (isPlaying(audio2)) {
+    //     audio2.pause();
+    //     audio2.currentTime = 0;
+    //   }
+    //   audio1.play();
+    // };
+    // // Onmouseup
+    // changingImage.onmouseup = () => {
+    //   changingImage.src = './assets/pikachuJump1.png';
+    //   if (isPlaying(audio1)) {
+    //     audio1.pause();
+    //     audio1.currentTime = 0;
+    //   }
+    //   audio2.play();
+    // };
   } else {
     // Otherwise, change everything back
     changingImage.src = 'https://pbs.twimg.com/media/C5e2Al2UYAExILo.jpg';
